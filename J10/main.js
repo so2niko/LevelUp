384c2a7806eda6831b9528c5418470d0108fcb21
Daria Shutova <dariashutova19@gmail.com>
2022-07-28 17:03:32 +0300

    added timer

--- J10/main.js
+++ J10/main.js
@@ -1,26 +1,47 @@
 let seconds = document.querySelector(".seconds");
 let minutes = document.querySelector('.minutes');
 let hours = document.querySelector('.hours');
+let sec = 0;
+let hrs = 0;
+let min = 0;
 const startButton = document.querySelector('.start');
 const stopButton = document.querySelector('.stop');
 const pauseButton = document.querySelector('.pause');
 const circleButton = document.querySelector('.circle');
 
 
-seconds.textContent = "58";
-// startButton.onclick = timerStart()
-// stopButton.onclick = timerStop()
-// pauseButton.onclick = timerPause()
-// circleButton.onclick = doCircle()
-
-
+startButton.onclick = timerStart // Почему тут функция должна быть без скобок?
+stopButton.onclick = timerStop
+pauseButton.onclick = timerPause
+// circleButton.onclick = doCircle
 
 
 function timerStart() {
-    t = setTimeout(addTime(), 1000);
+    t = setInterval(addTime, 1000);
 }
 
-// function addTime() {
+function addTime() {
+    sec++;
+    seconds.innerHTML = sec
+    minutes.innerHTML = min
+    hours.innerHTML = hrs
 
-// }
+    if (sec >= 59) {
+        sec = 0;
+        min++;
+        if (min >= 59) {
+            min = 0;
+            hrs++;
+        }
+    }
+}
+function timerPause() {
+    clearTimeout(t);
+}
 
+function timerStop() {
+    clearTimeout(t);
+    seconds.innerHTML = 0
+    minutes.innerHTML = 0
+    hours.innerHTML = 0
+}
